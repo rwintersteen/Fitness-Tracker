3051727a3a01e238877936f1cce2ee9b991f5475
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-20 08:55:29 -0700

    Set up server code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "", { useNewUrlParser: true });
+
+app.listen(PORT, () => 
+    console.log(`App is now listening at http://localhost:${PORT}!`))
