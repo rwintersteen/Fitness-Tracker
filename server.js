e74aa6914782f920bf8040d276ebcdd79c1529bc
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-25 10:37:17 -0700

    Rewrote model index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
