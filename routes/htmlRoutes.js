463e37012f6e342a8445a96e59005771f946087a
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-25 14:17:37 -0700

    Added res status to html routes and default page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,18 @@ const path = require('path');
 module.exports = function(app) {
 
 app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/exercise.html'))
+    res.sendFile(path.join(__dirname, '/public/exercise.html'));
+    res.status(200);
 });
 
 app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/stats.html'))
+    res.sendFile(path.join(__dirname, '/public/stats.html'));
+    res.status(200);
 });
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.status(200);
+})
 
 };
