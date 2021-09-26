42fe21f059c71c7a8ced347b0eb2a89efa3ab233
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-26 00:56:55 -0700

    Deploying

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,14 @@ const router = express.Router();
 
 router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', '/exercise.html'));
-    res.status(200);
 });
 
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', '/stats.html'));
-    res.status(200);
 });
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', '/index.html'));
-    res.status(200);
 })
 
 module.exports = router;
