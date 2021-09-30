d4beb9fa8fa81b04bc6b3edfb65b8fbaa8394718
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-09-29 19:05:45 -0700

    Used a const for mongodb uri

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 8000 || process.env.PORT;
 const mongoose = require('mongoose');
 const router = require('./routes');
 const path = require('path');
+const mongoURI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/workout'
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: false } ));
@@ -13,7 +14,7 @@ app.use(express.urlencoded( {extended: false } ));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
 
-mongoose.connect(process.env.MONGO_DB_URI, { 
+mongoose.connect(process.env.mongoURI, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }).then( () => {
